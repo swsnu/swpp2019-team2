eb64ae9ecddaa35889b5c7581ec37746f98e54f4
Daseul <das425@naver.com>
2019-10-31 23:56:24 +0900

    :sparkles:Remove redundant file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MainPage from './Containers/MainPage';
-import Search from './Containers/Search';
-import LogIn from './Containers/LogIn';
 import {Route, Redirect,Switch} from 'react-router-dom';
 import {ConnectedRouter} from 'connected-react-router';
 import BudgetSearch from './Containers/BudgetSearch';
@@ -15,11 +12,11 @@ function App(props) {
     <ConnectedRouter history = {props.history}>
         <div className="App">
         <Switch>
-          <Route path = '/login' exact component = {LogIn} />
-          {/*<Route path = '/signup' exact component = {} />
-          <Route path = '/mypage' exact component = {} />*/}
+          {/*<Route path = '/login' exact component = {LogIn} />
+          <Route path = '/signup' exact component = {} />
+          <Route path = '/mypage' exact component = {} />
           <Route path = '/main' exact component = {MainPage} />
-          <Route path = '/search' exact component = {Search} />
+          <Route path = '/search' exact component = {Search} />*/}
           <Route path = '/budget' exact component = {BudgetSearch} />
           <Route path = '/skintone' exact component = {SkinTone} />
           {/*<Route path = '/skintone/result' exact component = {} />
