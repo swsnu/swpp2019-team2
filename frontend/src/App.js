13e1359d6c7c7854f0cb7995bf0f484dd972d0f8
Daseul <das425@naver.com>
2019-11-01 01:24:12 +0900

    :sparkles: fix App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Search from './Containers/Search';
 import LogIn from './Containers/LogIn';
 import {Route, Redirect,Switch} from 'react-router-dom';
 import {ConnectedRouter} from 'connected-react-router';
+import BudgetSearch from './Containers/BudgetSearch';
+import SkinTone from './Containers/SkinTone';
 
 
 function App(props) {
@@ -14,8 +16,14 @@ function App(props) {
         <div className="App">
         <Switch>
           <Route path = '/login' exact component = {LogIn} />
+          {/*<Route path = '/signup' exact component = {} />
+          <Route path = '/mypage' exact component = {} />*/}
           <Route path = '/main' exact component = {MainPage} />
           <Route path = '/search' exact component = {Search} />
+          <Route path = '/budget' exact component = {BudgetSearch} />
+          <Route path = '/skintone' exact component = {SkinTone} />
+          {/*<Route path = '/skintone/result' exact component = {} />
+          <Route path = '/sale' exact component = {} /> */}
           <Redirect path='*' to='/login' />
         </Switch>
       </div>
