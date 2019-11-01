41b66b515c632b6181f954220335683e67be22b0
Aeri Cho <aeri33590@gmail.com>
2019-11-01 22:40:36 +0900

    modify detail for lip/brand model

--- backend/brand/models.py
+++ backend/brand/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Brand(models.Model):
     name = models.CharField(max_length=20)
     url = models.TextField()
+
+    def __str__(self):
+        return self.name
--- backend/lip/models.py
+++ backend/lip/models.py
@@ -1,7 +1,48 @@
 from django.db import models
-
+from brand import models as brand_models
 # Create your models here.
 
 class Lip(models.Model):
+    STICK = 'S'
+    GLOSS = 'G'
+    BALM = 'B'
+    TINT = 'T'
+    CATEGORY = (
+        (STICK,'Stick'),
+        (GLOSS,'Gloss'),
+        (BALM,'Balm'),
+        (TINT,'Tint'),
+    )
     name = models.CharField(max_length=20)
     price = models.IntegerField()
+    brand = models.ForeignKey(
+        brand_models.Brand,
+        on_delete=models.CASCADE
+    )
+    category = models.CharField(
+        max_length=1,
+        choices=CATEGORY,
+    )
+
+    def __str__(self):
+        return self.name
+
+class LipOption(models.Model):
+    RED = "RD"
+    CORAL = "CR"
+    PINK = "PK"
+    ORANGE = "OR"
+    COLOR = (
+        (RED , "Red"),
+        (CORAL , "Coral"),
+        (PINK, "Pink"),
+        (ORANGE, "Orange"),
+    )
+    color = models.CharField(
+        max_length=2,
+        choices=COLOR
+    )
+    product = models.ForeignKey(
+        Lip,
+        on_delete=models.CASCADE
+    )
