90b8eeb5cdd74b5d3d147658a533718b70757a33
Aeri Cho <aeri33590@gmail.com>
2019-11-03 02:01:24 +0900

    🚧 Implement Spider for Aritaum

--- backend/crawl/pipelines.py
+++ backend/crawl/pipelines.py
@@ -5,7 +5,25 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+from lip.models import Lip
+from brand.models import Brand
+
 
 class CrawlPipeline(object):
+    def __init__(self):
+            self.ids_seen = set()
+    
     def process_item(self, item, spider):
-        return item
+        if item["crawled"] == "brand":
+            self.process_brand(item)
+        elif item["crawled"] == "product":
+            item.save()
+            return item
+    
+    def process_brand(self, item):
+        if item["name"] not in self.ids_seen and len(Brand.objects.filter(name=item["name"])) == 0:
+            self.ids_seen.add(item["name"])
+            item.save()
+            return item
+        else:
+            return item
---
+++ backend/crawl/spiders/aritaumSpider.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from crawl.items import LipProduct, Brand
+from selenium import webdriver
+from brand.models import Brand as Brand_db
+
+class AritaumShopSpider(scrapy.Spider):
+    name = 'aritaum'
+    
+    def __init__(self):
+        scrapy.Spider.__init__(self)
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        self.browser = webdriver.Chrome('chromedriver',chrome_options=options)
+    
+    def start_requests(self):
+        link = 'link'
+        idx = 'category'
+        
+        urls = [
+            {idx: "lip",
+            link:"https://www.aritaum.com/shop/pr/shop_pr_product_list.do?i_sCategorycd1=CTGA2000&i_sCategorycd2=CTGA2200"},
+            ]
+        for url in urls:
+            yield scrapy.Request(url=url[link], meta={idx:url[idx]}, callback=self.parse_brand)
+
+    def parse_brand(self, response):
+        driver = self.browser
+        driver.get(response.url)
+        
+        brand_list = driver.find_elements_by_name("tagging_brandNm")
+        
+        for brand_name in brand_list:
+            yield Brand(name=brand_name.get_property("value"), crawled="brand")
+            
+        product_list = driver.find_elements_by_xpath("//*[@id='ul_prod_list']/li")
+        yield scrapy.Request(url=response.url, meta={"product":product_list}, callback=self.parse_product, dont_filter=True)
+
+        
+    def parse_product(self, response):
+        product_list = response.meta["product"]
+        category_EN = {
+            "립스틱":"S",
+            "립글로즈":"G",
+            "립케어/립밤":"B",
+            "립틴트":"T",
+            }
+        for item in product_list:
+            product_name = item.find_element_by_name("tagging_productNm").get_property("value")
+            brand_name = item.find_element_by_name("tagging_brandNm").get_property("value")
+            price = item.find_element_by_name("tagging_price").get_property("value") 
+            category_KO = item.find_element_by_name("tagging_category").get_property("value")[12:]
+            category = category_EN[category_KO]
+            brand = Brand_db.objects.filter(name = brand_name)
+            thumb_url = item.find_element_by_css_selector("div.product-thumb img").get_property("src")
+            
+            yield LipProduct(
+                name=product_name,
+                price=price,
+                brand=brand[0],
+                category=category,
+                img_url=thumb_url,
+                crawled="product"
+            )
+            
+            color_range = item.find_elements_by_xpath("//*[@id='ul_prod_list']/li[1]/div/div[1]/div/div[1]/div/ul/li")
+            for color in color_range:
+                color_img_src = color.find_element_by_tag_name("img").get_property("src")
+                color_name = color.find_element_by_tag_name("label").get_attribute("data-tooltip")
+            
+    
