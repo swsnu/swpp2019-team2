c6a1603b9cb2169c412540496a0687229880363d
lllY2Klll <knhh4555@snu.ac.kr>
2019-11-04 23:49:28 +0900

    budget test modified

--- frontend/src/Containers/BudgetSearch.test.js
+++ frontend/src/Containers/BudgetSearch.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { Route, Redirect, Switch } from 'react-router-dom';
