6663b212a9041edfdb5769b71ae7fa90eb4636da
Aeri Cho <aeri33590@gmail.com>
2019-11-09 14:30:03 +0900

    ✅ Add test for LipOption model

--- backend/test_models.py
+++ backend/test_models.py
@@ -1,15 +1,19 @@
-from lip.models import Lip
+from lip.models import Lip, LipOption
 from brand.models import Brand
 import unittest
 
 class ItemTest(unittest.TestCase):
     
     def test_lip_class(self):
-        i = Lip(name="tmp_name",)
+        i = Lip(name="tmp_name")
         self.assertEqual(str(i), i.name)
 
     def test_brand_class(self):
         i = Brand(name_ko="tmp_name")
         self.assertEqual(str(i), i.name_ko)
 
+    def test_lipoption_class(self):
+        i = Lip(name="tmp_lip")
+        o = LipOption(product=i, optionName="tmp_option")
+        self.assertEqual(str(o), str(i)+" "+ o.optionName)
     
