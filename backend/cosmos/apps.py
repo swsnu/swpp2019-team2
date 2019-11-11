f97a2e51c672cba29aab3dae9f75c15a5e95c098
Aeri Cho <aeri33590@gmail.com>
2019-11-12 00:49:17 +0900

    cosmos -> user 모듈 명 변경
    - 화장품 관련 모델 삭제

--- backend/cosmos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CosmosConfig(AppConfig):
-    name = 'cosmos'
--- backend/user/views.py
+++ backend/user/views.py
@@ -6,40 +6,13 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.contrib.auth import login, authenticate, logout
 from django.views.decorators.csrf import csrf_exempt
-#from django.core import serializers
-#from rest_framework import status
-#from rest_framework.views import APIView
-#from rest_framework.response import Response
-#from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView
-#from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.db import IntegrityError
-from .models import Lip
-
-
-def lip(request):
-    """LIP MODEL FUNCTION"""
-    if request.method == 'GET':
-        if request.user.is_authenticated:  # 로그인 확인
-            lip_all_list = Lip.objects.all()
-            result = list(
-                map(
-                    lambda lip: {
-                        "name": lip.name,
-                        "category": lip.category,
-                        "price": lip.price,
-                        "thumbnail": lip.thumbnail,
-                        "color": lip.color.name,
-                        "brand": lip.brand.name},
-                    lip_all_list))
-            return JsonResponse(result, safe=False)
-        return HttpResponse(status=401)
-
-    return HttpResponseNotAllowed(['GET'])
 
 
 @csrf_exempt
 def signup(request):
     """SIGNUP FUNCTION"""
+    print("signup")
     if request.method == 'GET':
         user_all_list = [cosmos for cosmos in User.objects.all().values()]
         return JsonResponse(user_all_list, safe=False)
