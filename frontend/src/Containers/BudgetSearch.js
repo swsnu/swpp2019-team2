2edc301bc73aa1dc49647960bab8df01a6e42495
lllY2Klll <knhh4555@snu.ac.kr>
2019-11-18 05:40:01 +0900

    changed budget-range select logic

--- frontend/src/Containers/BudgetSearch.js
+++ frontend/src/Containers/BudgetSearch.js
@@ -3,11 +3,25 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import './BudgetSearch.css';
 import swal from 'sweetalert';
+import Select from 'react-select';
 import { connect } from 'react-redux';
 import * as actionCreators from '../store/actions/index';
 import arrow from '../image/화살표.png';
 import CheckBox from './CheckBox';
 import ItemDisplay from './ItemDisplay';
+
+const options = [
+  { value: 0, label: '0원 ~ 5000원' }, // 삭제가능
+  { value: 1, label: '5000원 ~10000원' },
+  { value: 2, label: '10000원 ~ 15000원' },
+  { value: 3, label: '15000원 ~ 20000원' },
+  { value: 4, label: '20000원 ~ 25000원' },
+  { value: 5, label: '25000원 ~ 30000원' },
+  { value: 6, label: '30000원 ~ 35000원' },
+  { value: 7, label: '35000원 ~ 40000원' },
+  { value: 8, label: '40000원 ~ 45000원' },
+  { value: 9, label: '45000원 ~ 50000원' },
+];
 
 class BudgetSearch extends Component {
   constructor(props) {
@@ -26,20 +40,8 @@ class BudgetSearch extends Component {
       list5: [{ name: 'e1', price: 8400 }, { name: 'e2', price: 10800 }, { name: 'e3', price: 13200 }, { name: 'e4', price: 15600 }, { name: 'e5', price: 18000 }, { name: 'e6', price: 20400 }, { name: 'e7', price: 22800 }, { name: 'e8', price: 25200 }, { name: 'e9', price: 27600 }, { name: 'e10', price: 30000 }],
       combi: [],
       show: false,
-      itemNum: 4,
+      itemNum: 2,
       budgetRange: null,
-      checked: [
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-      ],
     };
   }
 
@@ -69,25 +71,11 @@ class BudgetSearch extends Component {
     }
   }
 
-  handleChange = (num) => {
-    const selected = [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    ];
-    if (this.state.checked[num] === true) {
-      this.setState({ checked: selected });
+  handleChange = (selected) => {
+    if (selected === null) {
       this.setState({ budgetRange: null });
     } else {
-      selected[num] = true;
-      this.setState({ checked: selected });
+      const num = selected.value;
       const chosenBudgetRange = [5000 * num, 5000 * (num + 1)];
       this.setState({ budgetRange: chosenBudgetRange });
     }
@@ -214,7 +202,7 @@ class BudgetSearch extends Component {
 
   render() {
     const {
-      checked, id, itemNum, find1, find2, find3, find4, find5, show, combi,
+      id, itemNum, find1, find2, find3, find4, find5, show, combi,
     } = this.state;
     const { isAuthenticated } = this.props;
     let redirect = null;
@@ -239,6 +227,14 @@ class BudgetSearch extends Component {
             <button id="my-page-button" type="button" onClick={() => this.mypageHandler(id)}>My Page</button>
           </div>
         </div>
+        <h4>{strBudget}</h4>
+        <Select
+          isClearable
+          placeholder="select budget..."
+          options={options}
+          onChange={(selected) => this.handleChange(selected)}
+          styles={{ width: '70%' }}
+        />
         <div className="item_input">
           <h4>{strNumItems}</h4>
           <input type="text" name="item_val" readOnly value={itemNum} />
@@ -254,46 +250,11 @@ class BudgetSearch extends Component {
         {itemNum > 2 && (<CheckBox className="checkbox" id="checkbox3" findUrl={(url) => this.findThird(url)} />)}
         {itemNum > 3 && (<CheckBox className="checkbox" id="checkbox4" findUrl={(url) => this.findFourth(url)} />)}
         {itemNum > 4 && (<CheckBox className="checkbox" id="checkbox5" findUrl={(url) => this.findFifth(url)} />)}
-        <div className="Budget_input">
-          <h4>{strBudget}</h4>
-          <div className="input1">
-            <input type="checkbox" name="budget_range" id="range1" checked={checked[0]} onChange={() => this.handleChange(0)} />
-            0원 이상 ~ 5000원 미만
-
-            <input type="checkbox" name="budget_range" id="range2" checked={checked[1]} onChange={() => this.handleChange(1)} />
-            5000원 이상 ~ 10000원 미만
-
-            <input type="checkbox" name="budget_range" id="range3" checked={checked[2]} onChange={() => this.handleChange(2)} />
-            10000원 이상 ~ 15000원 미만
-
-            <input type="checkbox" name="budget_range" id="range4" checked={checked[3]} onChange={() => this.handleChange(3)} />
-            15000원 이상 ~ 20000원 미만
-
-            <input type="checkbox" name="budget_range" id="range5" checked={checked[4]} onChange={() => this.handleChange(4)} />
-            20000원 이상 ~ 25000원 미만
-          </div>
-          <div className="input2">
-            <input type="checkbox" name="budget_range" id="range6" checked={checked[5]} onChange={() => this.handleChange(5)} />
-            25000원 이상 ~ 30000원 미만
-
-            <input type="checkbox" name="budget_range" id="range7" checked={checked[6]} onChange={() => this.handleChange(6)} />
-            30000원 이상 ~ 35000원 미만
-
-            <input type="checkbox" name="budget_range" id="range8" checked={checked[7]} onChange={() => this.handleChange(7)} />
-            35000원 이상  ~ 40000원 미만
-
-            <input type="checkbox" name="budget_range" id="range9" checked={checked[8]} onChange={() => this.handleChange(8)} />
-            40000원 이상  ~ 45000원 미만
-
-            <input type="checkbox" name="budget_range" id="range10" checked={checked[9]} onChange={() => this.handleChange(9)} />
-            45000원 이상  ~ 50000원 미만
-          </div>
-          <div className="button">
-            <button id="combine-cosmetics-button" type="submit" onClick={() => this.confirmHandler()}>Combine Cosmetics</button>
-          </div>
-          <div>
-            <button type="button" id="reset-result" onClick={() => this.setState({ combi: [] })}> 초기화 </button>
-          </div>
+        <div className="button">
+          <button id="combine-cosmetics-button" type="submit" onClick={() => this.confirmHandler()}>Combine Cosmetics</button>
+        </div>
+        <div>
+          <button type="button" id="reset-result" onClick={() => this.setState({ combi: [] })}> 초기화 </button>
         </div>
         {show && (<ItemDisplay combinations={combi} />)}
       </div>
