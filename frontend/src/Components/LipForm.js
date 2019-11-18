6d7999f3bc9bd6bff8812df90240fe1dcc7f843d
Aeri Cho <aeri33590@gmail.com>
2019-11-19 03:22:29 +0900

    💄LipForm UI 수정

--- frontend/src/Components/LipForm.js
+++ frontend/src/Components/LipForm.js
@@ -1,52 +1,46 @@
 import React from 'react';
+import './LipForm.css';
 
-const LipForm = (props) => (
-  <div className="LipForm">
-    <div>
-
-
-      <section className="id">
-        <h4 className="Lip_id">{props.id}</h4>
-      </section>
-
-      <section className="thumbnail">
-        <img height="200" width="200" src={props.thumbnail} alt="new" />
-      </section>
-
-      <section className="name">
-        <h4 id="Lip_name">{props.name}</h4>
-      </section>
-
-
-      <section className="price">
-        <h4 className="Lip_price">
+const LipForm = (props) => {
+  const {
+    imgUrl, name, price, category, brand, form,
+  } = props;
+  return (
+    <li className="LipContainer">
+      <div>
+        <section className="thumbnail">
+          <img height="200" width="200" src={imgUrl} alt="new" />
+        </section>
+        <section className="name">
+          <h4 id="Lip_name">{name}</h4>
+        </section>
+        <section className="price">
+          <h4 className="Lip_price">
 가격:&emsp;
-          {props.price}
+            {price}
 원
-        </h4>
-      </section>
-
-      <section className="category">
-        <h4 className="Lip_category">
+          </h4>
+        </section>
+        <section className="category">
+          <h4 className="Lip_category">
 카테고리:&emsp;
-          {props.category}
-        </h4>
-      </section>
-
-      <section className="category">
-        <h4 className="Lip_form">
+            {category}
+          </h4>
+        </section>
+        <section className="category">
+          <h4 className="Lip_form">
 폼:&emsp;
-          {props.form}
-        </h4>
-      </section>
-
-      <section className="brand">
-        <h4 className="Lip_brand">
+            {form}
+          </h4>
+        </section>
+        <section className="brand">
+          <h4 className="Lip_brand">
 브랜드:&emsp;
-          {props.brand}
-        </h4>
-      </section>
-    </div>
-  </div>
-);
+            {brand}
+          </h4>
+        </section>
+      </div>
+    </li>
+  );
+};
 export default LipForm;
