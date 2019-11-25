5268d0e3f7afcfbf67ca49c352d94a0304b82bfe
Aeri Cho <aeri33590@gmail.com>
2019-11-25 18:34:03 +0900

    ✨ SubcolorSelection 추가

--- frontend/src/Components/DetailCategory.js
+++ frontend/src/Components/DetailCategory.js
@@ -13,6 +13,57 @@ class DetailCategory extends React.Component {
     const LargeCategory = CATEGORY[id];
     const keys = Object.keys(LargeCategory);
     const res = keys.map((key) => {
+      if (key === "color") {
+        const colorKeys = Object.keys(LargeCategory["color"]);
+        const colors = colorKeys.map((colorKey) => {
+          const subcolorID = `${key}=${LargeCategory["color"][colorKey][0]}&`;
+          const onClick = e => {
+            if (e.target.checked){
+              document.querySelector(`ul.detail-subcolor-visual#${e.target.value}`).style="display:inline-block;"
+            }
+            else 
+              document.querySelector(`ul.detail-subcolor-visual#${e.target.value}`).style="display:none;"
+          }
+          return (
+            <label className="selectionValue" key={colorKey} htmlFor={subcolorID}>
+            {colorKey}
+            <input type="checkbox" onClick={onClick} value={colorKey} id={subcolorID} />
+          </label>
+          )
+        });
+        const subcolors = colorKeys.map((colorKey) => {
+          const colorData = LargeCategory["color"][colorKey];
+          const detailColors = colorData[1];
+          
+          const onClick = e => {
+            const label = e.target.parentElement;
+            const bg = label.style.backgroundColor;
+            label.style = e.target.checked ? 
+            `opacity:40%; background-color:${bg};`:
+            `opacity:100%; background-color:${bg};`
+          }
+          const detailColorList = detailColors.map((hex) => {
+            const style = { backgroundColor: hex };
+            const id=`sub_color=${hex}&`
+            return (<label  htmlFor={id} className="color-selection-chip" key={hex} style={style}>
+              <input onClick={onClick} className="color-selection-input-box" id={id}type="checkbox"/>
+              </label>);
+          });
+          const ulColorKey = `subcolor-${colorData[0]}`;
+          const style={ display: 'none' };
+          return (<ul key={ulColorKey} className="detail-subcolor-visual" style={style} id={colorKey}>{detailColorList}</ul>);
+        })
+        
+        return (
+          <div className="detail-sub-category" key={key}>
+            <div className="detail-sub-color-category">
+          <h4>{key}</h4>
+          <div>{colors}</div>
+          </div>
+          <div className="sub-color-visual">{subcolors}</div>
+        </div>
+        )
+      }
       const subkeys = Object.keys(LargeCategory[key]);
       const subres = subkeys.map((subkey) => {
         const subkeyId = `${key}=${LargeCategory[key][subkey]}&`;
