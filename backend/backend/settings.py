85046ec88295feceb9c970761d3f47cead62abb7
Ubuntu <cosmos-swpp-admin@swpp2019-team2.ff1q5pmmpuhexmldqmqlqkkapc.syx.internal.cloudapp.net>
2019-12-04 15:01:43 +0000

    Change Code for Deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm8k3-*+ua+&ba4h45&zi7l!)v@6s6zid*=&0fgj3z6dwad5!8d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.141.1.157', 'www.csmscsms.shop', 'localhost', '127.0.0.1']
 
 
 # Application definition
