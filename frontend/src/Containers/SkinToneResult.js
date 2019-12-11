5e3a0302f95a6b8a6548716d364ea7ee60767867
Aeri Cho <aeri33590@gmail.com>
2019-12-11 21:21:57 +0900

    Import Header in skintone result

--- frontend/src/Containers/SkinToneResult.js
+++ frontend/src/Containers/SkinToneResult.js
@@ -3,142 +3,92 @@ import { Redirect } from 'react-router-dom';
 import './SkinToneResult.css';
 import { connect } from 'react-redux';
 import * as actionCreators from '../store/actions/index';
+import Header from '../Components/Header';
 
 class SkinToneResult extends Component {
   constructor(props) {
     super(props);
     this.state = {
       result: '',
-      rgb_result: '',
+      rgbResult: '',
     };
   }
 
   componentDidMount() {
-    this.props.onTryAutoSignup();
-    this.setState({ result: `rgb(${this.props.ML.result_r},${this.props.ML.result_g},${this.props.ML.result_b})` });
-    this.setState({ rgb_result: `rgb( ${this.props.ML.result_r.toFixed(5)} , ${this.props.ML.result_g.toFixed(5)} , ${this.props.ML.result_b.toFixed(5)} )` });
+    const { ML, onTryAutoSignup } = this.props;
+    onTryAutoSignup();
+    this.setState({ result: `rgb(${ML.result_r},${ML.result_g},${ML.result_b})` });
+    this.setState({ rgbResult: `rgb( ${ML.result_r.toFixed(5)} , ${ML.result_g.toFixed(5)} , ${ML.result_b.toFixed(5)} )` });
     /* if (this.props.selectedUser) {
         this.setState({ id: this.props.selectedUser.id });
       } */
   }
 
-      logoutHandler = () => {
-        this.props.Logout();
-        this.props.onTryAutoSignup();
-      }
-
-      mypageHandler = (id) => {
-        this.props.history.replace(`../mypage/${id}`);
-      }
-
-      searchHandler = () => {
-        this.props.history.replace('../search');
-      };
-
-      budgetHandler = () => {
-        this.props.history.replace('../budget');
-      };
-
-      toneHandler = () => {
-        this.props.history.replace('../skintone');
-      };
-
-      saleHandler = () => {
-        this.props.history.replace('../sale');
-      };
-
-      render() {
-        // const colorbarSrc = `http://52.141.1.157:8000/media/output/colorbar${localStorage.getItem('nickname')}.png`;
-        const src = `/media/output/colorbar${localStorage.getItem('nickname')}.png`;
-        let redirect = null;
-        let infoString = '';
-        if (this.state.render === false) {
-          this.props.user.map((res) => (
-            this.setState({ nickName: res.nick_name })
-          ));
-          this.setState({ render: true });
-        }
-        infoString = `Hello, ${localStorage.getItem('nickname')}!`;
-        if (!localStorage.getItem('token')) {
-          window.alert('로그인을 먼저 진행해주세요');
-          redirect = <Redirect to="/login" />;
-        }
-        const productUrl = '';
-        const FoundationResult = (
-          <a target="_blank" rel="noopener noreferrer" className="productItem" href={productUrl}>
-            <section className="thumbnail">
-              <img height="200" width="200" src={this.props.ML.product.img_url} alt="new" />
-            </section>
-            <section className="name_info_text">
-              {this.props.ML.base}
-              {' / '}
-              {this.props.ML.product.brand}
-            </section>
-            <section className="price_info_text">
+  render() {
+    let redirect = null;
+    if (!localStorage.getItem('token')) {
+      window.alert('로그인을 먼저 진행해주세요');
+      redirect = <Redirect to="/login" />;
+    }
+    const src = `/media/output/colorbar${localStorage.getItem('nickname')}.png`;
+    const productUrl = '';
+    const { ML, history } = this.props;
+    const { result, rgbResult } = this.state;
+    const FoundationResult = (
+      <a target="_blank" rel="noopener noreferrer" className="productItem" href={productUrl}>
+        <section className="thumbnail">
+          <img height="200" width="200" src={ML.product.img_url} alt="new" />
+        </section>
+        <section className="name_info_text">
+          {ML.base}
+          {' / '}
+          {ML.product.brand}
+        </section>
+        <section className="price_info_text">
 가격:&emsp;
-              {this.props.ML.product.price}
+          {ML.product.price}
 원
-            </section>
-          </a>
-        );
-        if (!this.props.isAuthenticated) {
-          redirect = <Redirect to="/login" />;
-        }
-        const id = this.state;
-        return (
-          <div className="SkinToneResult">
-            {redirect}
-            {/* <div id = "LOGO">
-                      <img src = {logo} alt = "COSMOS" width = "100" />
-                  </div> */}
-            <div className="upperbar">
-              <h1>Skin Tone Analysis</h1>
-              <div className="buttons">
-                <button id="log-out-button" type="button" onClick={() => this.logoutHandler()}>Log-Out</button>
-                <button id="my-page-button" type="button" onClick={() => this.mypageHandler(id)}>My Page</button>
-              </div>
-              {infoString}
+        </section>
+      </a>
+    );
+    return (
+      <div className="SkinToneResult">
+        {redirect}
+        <Header history={history} selected={2} />
+        <div className="Content">
+          <div className="resultbox">
+            <h2>Result</h2>
+            <div className="result">
+              <h4>Skintone</h4>
+              <img id="colorbar" src={src} alt="ColorBar" width="300px" height="300px" margin="auto" />
+              <div
+                className="MLresult"
+                style={{
+                  backgroundColor: result, width: 80, height: 80,
+                }}
+              />
+              <div>{rgbResult}</div>
             </div>
-            <div className="Menubar">
-              <div><button id="Searchmenu" type="button" onClick={() => this.searchHandler()}>Search-Tag</button></div>
-              <div><button id="Budgetmenu" type="button" onClick={() => this.budgetHandler()}>Budget-Search</button></div>
-              <div><button id="Tonemenu" type="button" onClick={() => this.toneHandler()}>Tone-Analysis</button></div>
-              <div><button id="Salemenu" type="button" onClick={() => this.saleHandler()}>Sale-Info</button></div>
-            </div>
-            <div className="resultbox">
-              <h2>Result</h2>
-              <div className="result">
-                <h4>Skintone</h4>
-                <img id="colorbar" src={src} alt="ColorBar" width="300px" height="300px" margin="auto" />
-                <div
-                  className="MLresult"
-                  style={{
-                    backgroundColor: this.state.result, width: 80, height: 80,
-                  }}
-                />
-                <div>{this.state.rgb_result}</div>
-              </div>
-              <div className="recommendation">
-                <h4>User ID</h4>
-                <div>{this.props.ML.user_id}</div>
-                <h4>Foundation Recommendation</h4>
-                <div>{FoundationResult}</div>
-                <div className="retry"><button id="Retry" type="button" onClick={() => this.toneHandler()}>Re-try!</button></div>
-              </div>
+            <div className="recommendation">
+              <h4>User ID</h4>
+              <div>{ML.user_id}</div>
+              <h4>Foundation Recommendation</h4>
+              <div>{FoundationResult}</div>
+              <div className="retry"><button id="Retry" type="button" onClick={() => this.toneHandler()}>Re-try!</button></div>
             </div>
           </div>
-        );
-      }
+        </div>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state) => ({
-  isAuthenticated: state.cosmos.token != null,
   loading: state.cosmos.loading,
   error: state.cosmos.error,
   ML: state.cosmos.ML,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  Logout: () => dispatch(actionCreators.logout()),
   onTryAutoSignup: () => dispatch(actionCreators.authCheckState()),
   run_analysis: (id) => dispatch(actionCreators.runAnalysis({ userID: id })),
 });
