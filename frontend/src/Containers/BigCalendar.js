2abbd81571038462521c73ca3053b17fd61b9dfd
lllY2Klll <knhh4555@snu.ac.kr>
2019-12-13 14:26:44 +0900

    sale info added

--- frontend/src/Containers/BigCalendar.js
+++ frontend/src/Containers/BigCalendar.js
@@ -1,64 +1,100 @@
+/* eslint-disable max-len */
+/* eslint-disable react/button-has-type */
 import React, { Component } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import swal from 'sweetalert';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './BigCalendar.css';
 
 
 const localizer = momentLocalizer(moment);
+const formats = {
+  monthHeaderFormat: 'YYYY. MM',
+};
+
+const MAC = [
+  {
+    start: new Date('2019-12-09'),
+    end: new Date('2019-12-15'),
+    title: 'MAC',
+    header: '20주년 맥 서울파우치 이벤트',
+    detail: '장소: 전국 맥 백화점 매장\n내용: 맥20 아이코닉 제품 포함 8만원 이상 구매시 \n 맥 서울파우치 증정',
+  },
+];
+const ARITAUM = [
+  {
+    start: new Date('2019-12-11'),
+    end: new Date('2019-12-14'),
+    title: 'ARITAUM',
+    header: '2019년 결산 세일',
+    detail: '19년 아리따움 연말 결산 세일 혜택 \n 일반: 15% ~ 60% \n 스마트클럽: 25% ~ 60%',
+  },
+];
+const MAMONDE = [
+  {
+    start: new Date('2019-12-09'),
+    end: new Date('2019-12-31'),
+    title: 'MAMONDE',
+    header: '아리따움 완판 기념 앵콜 요청 이벤트',
+    detail: '1. 19500원에 1일 1팩 (환절기 고보습 마스크 등) \n 2. 클리어런스 팩 최대 70% 할인',
+  },
+];
+const DEARDAHLIA = [
+  {
+    start: new Date('2019-12-09'),
+    end: new Date('2019-12-31'),
+    title: 'DEARDAHLIA',
+    header: '디어달리아 신제품 출시기념 한정 GIFT',
+    detail: '디어달리아 메이크업 제품 구매 시 \n 미니벨벳 립 무스 구아바 1ml 증정 \n(재고 소진 시까지)',
+  },
+];
+const PRIMERA = [
+  {
+    start: new Date('2019-11-30'),
+    end: new Date('2019-12-31'),
+    title: 'PRIMERA',
+    header: 'Happy 아리따움, Merry 프리메라',
+    detail: '1. 알파인 베리 라인 제품 15% 할인 \n 2. 프리메라 알파인 베리 워터리 제품 구매 시 \n 프리메라 기초 스킨케어 4종 KIT 증정',
+  },
+];
+
+const buttonStyle = {
+  width: 120,
+  height: 30,
+  borderRadius: 8,
+  marginBottom: 10,
+  justifyContent: 'center',
+};
 
 class BigCalendar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: [
-        {
-          start: new Date(),
-          end: new Date(moment().add(1, 'days')),
-          title: 'Dummy sale info1',
-          detail: 'Dummy detail1',
-        },
-        {
-          start: new Date(moment().add(10, 'days')),
-          end: new Date(moment().add(14, 'days')),
-          title: 'Dummy sale info2',
-          detail: 'Dummy detail2',
-        },
-        {
-          start: new Date('2019-12-05'),
-          end: new Date('2019-12-10'),
-          title: 'Dummy sale info3',
-          detail: 'Dummy detail3',
-        },
-        {
-          start: new Date('2019-12-09'),
-          end: new Date('2019-12-13'),
-          title: 'Dummy sale info4',
-          detail: 'Dummy detail4',
-        },
-        {
-          start: new Date('2019-12-21'),
-          end: new Date('2019-12-25'),
-          title: 'Dummy sale info5',
-          detail: 'Dummy detail5',
-        },
-      ],
+      selectedEvent: MAC,
     };
   }
 
-
   render() {
-    const { events } = this.state;
+    const { selectedEvent } = this.state;
     return (
-      <div className="Calendar" style={{ display: 'table-cell' }}>
+      <div className="Calendar" style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <button id="button1" style={buttonStyle} onClick={() => this.setState({ selectedEvent: MAC })}>MAC</button>
+          <button id="button2" style={buttonStyle} onClick={() => this.setState({ selectedEvent: ARITAUM })}>ARITAUM</button>
+          <button id="button3" style={buttonStyle} onClick={() => this.setState({ selectedEvent: MAMONDE })}>MAMONDE</button>
+          <button id="button4" style={buttonStyle} onClick={() => this.setState({ selectedEvent: DEARDAHLIA })}>DEARDAHLIA</button>
+          <button id="button5" style={buttonStyle} onClick={() => this.setState({ selectedEvent: PRIMERA })}>PRIMERA</button>
+        </div>
         <Calendar
           id="BigCalendar"
+          views={['month']}
           localizer={localizer}
           defaultDate={new Date()}
           defaultView="month"
-          events={events}
-          style={{ height: '80vh', width: '140vh', margin: 'auto' }}
-          onSelectEvent={(event) => swal(event.title, event.detail)}
+          formats={formats}
+          events={selectedEvent}
+          style={{ height: '70vh', width: '140vh', marginLeft: 10 }}
+          onSelectEvent={(event) => swal(event.header, event.detail)}
         />
       </div>
     );
@@ -66,3 +102,5 @@ class BigCalendar extends Component {
 }
 
 export default BigCalendar;
+
+// display: 'table-cell'  margin: 'auto'
--- frontend/src/Containers/BigCalendar.test.js
+++ frontend/src/Containers/BigCalendar.test.js
@@ -4,14 +4,72 @@ import BigCalendar from './BigCalendar';
 
 const stubEvent = {
   title: 'dummy title',
+  header: 'dummy header',
   detail: 'dummy detail',
 };
+const mockState = [
+  [
+    {
+      start: new Date('2019-12-09'),
+      end: new Date('2019-12-15'),
+      title: 'MAC',
+      header: '20주년 맥 서울파우치 이벤트',
+      detail: '장소: 전국 맥 백화점 매장\n내용: 맥20 아이코닉 제품 포함 8만원 이상 구매시 \n 맥 서울파우치 증정',
+    },
+  ],
+  [
+    {
+      start: new Date('2019-12-11'),
+      end: new Date('2019-12-14'),
+      title: 'ARITAUM',
+      header: '2019년 결산 세일',
+      detail: '19년 아리따움 연말 결산 세일 혜택 \n 일반: 15% ~ 60% \n 스마트클럽: 25% ~ 60%',
+    },
+  ],
+  [
+    {
+      start: new Date('2019-12-09'),
+      end: new Date('2019-12-31'),
+      title: 'MAMONDE',
+      header: '아리따움 완판 기념 앵콜 요청 이벤트',
+      detail: '1. 19500원에 1일 1팩 (환절기 고보습 마스크 등) \n 2. 클리어런스 팩 최대 70% 할인',
+    },
+  ],
+  [
+    {
+      start: new Date('2019-12-09'),
+      end: new Date('2019-12-31'),
+      title: 'DEARDAHLIA',
+      header: '디어달리아 신제품 출시기념 한정 GIFT',
+      detail: '디어달리아 메이크업 제품 구매 시 \n 미니벨벳 립 무스 구아바 1ml 증정 \n(재고 소진 시까지)',
+    },
+  ],
+  [
+    {
+      start: new Date('2019-11-30'),
+      end: new Date('2019-12-31'),
+      title: 'PRIMERA',
+      header: 'Happy 아리따움, Merry 프리메라',
+      detail: '1. 알파인 베리 라인 제품 15% 할인 \n 2. 프리메라 알파인 베리 워터리 제품 구매 시 \n 프리메라 기초 스킨케어 4종 KIT 증정',
+    },
+  ],
+];
 
 describe('<BigCalender />', () => {
   it('should show calander', () => {
     const component = shallow(<BigCalendar />);
     const wrapper = component.find('.Calendar');
     expect(wrapper.length).toBe(1);
+  });
+
+  it('should show calander', () => {
+    const component = shallow(<BigCalendar />);
+    let button;
+    for (let i = 1; i <= 5; i++) {
+      button = component.find(`#button${i}`);
+      button.simulate('click');
+      expect(component.state().selectedEvent).toEqual(mockState[i - 1]);
+    }
   });
 
   it('should show popup when select specific event', () => {
